9f800ca8c2ce09abf78beaef74ddbb755a0f14b8
James Wu <88938117+thejwuscript@users.noreply.github.com>
2023-10-17 22:39:50 +0900

    Use template system and remove hardcoded url

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Question
 from django.http import Http404
+from django.template import loader
 
 def index(request):                 
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
